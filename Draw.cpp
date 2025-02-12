6a768f3a114f7de26e6678895cc570d093f0ba56
AsakawaSakuto <k024g1017@g.neec.ac.jp>
2025-02-12 18:17:39 +0900

    vector2Intの修正

--- Draw.cpp
+++ Draw.cpp
@@ -1,5 +1,5 @@
 #include"Draw.h"
-void DrawBoxInt(Vector2Int pos, Vector2Int size, unsigned int color, bool kfillMode)
+void DrawBoxInt(Vector2_Int pos, Vector2_Int size, unsigned int color, bool kfillMode)
 {
 	if (kfillMode)
 	{
@@ -21,7 +21,7 @@ void DrawBoxFloat(Vector2 pos, Vector2 size, unsigned int color, bool kfillMode)
 		Novice::DrawBox(static_cast<int>(pos.x - size.x / 2.f), static_cast<int>(pos.y - size.y / 2.f), static_cast<int>(size.x), static_cast<int>(size.y), 0.f, color, kFillModeWireFrame);
 	}
 }
-void DrawEllipseInt(Vector2Int pos, int radius, unsigned int color, bool kfillMode)
+void DrawEllipseInt(Vector2_Int pos, int radius, unsigned int color, bool kfillMode)
 {
 	if (kfillMode)
 	{
--- Draw.h
+++ Draw.h
@@ -2,6 +2,12 @@
 #include"Novice.h"
 #include"Obj.h"
 #include"Vector2.h"
+
+struct Vector2_Int final {
+	int x;
+	int y;
+};
+
 /// <summary>
 /// int型用のDrawBox
 /// </summary>
@@ -9,7 +15,7 @@
 /// <param name="size">サイズXY</param>
 /// <param name="color">色</param>
 /// <param name="kfillMode">trueで塗りつぶしfalseで線のみ</param>
-void DrawBoxInt(Vector2Int pos, Vector2Int size, unsigned int color, bool kfillMode);
+void DrawBoxInt(Vector2_Int pos, Vector2_Int size, unsigned int color, bool kfillMode);
 
 /// <summary>
 /// float型用のDrawBox int型にcastして描画
@@ -27,7 +33,7 @@ void DrawBoxFloat(Vector2 pos, Vector2 size, unsigned int color, bool kfillMode)
 /// <param name="radius">半径</param>
 /// <param name="color">色</param>
 /// <param name="kfillMode">trueで塗りつぶしfalseで線のみ</param>
-void DrawEllipseInt(Vector2Int pos, int radius, unsigned int color, bool kfillMode);
+void DrawEllipseInt(Vector2_Int pos, int radius, unsigned int color, bool kfillMode);
 
 /// <summary>
 /// float型用のDrawEllipse int型にcastして描画
